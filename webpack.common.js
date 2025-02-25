280946d143bb79b6d533e19477df4357e785390f
Goran Jovic <goran1010jovic@gmail.com>
2025-02-25 21:06:28 +0100

    add fonts and more basic css styles

--- webpack.common.js
+++ webpack.common.js
@@ -39,16 +39,10 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-        generator: {
-          filename: "images/[name][ext]",
-        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
-        generator: {
-          filename: "fonts/[name][ext]",
-        },
       },
     ],
   },
